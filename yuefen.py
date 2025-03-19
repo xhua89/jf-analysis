bc68a4649a63f3b06ea21668aa03f46dfec36243
xhua89 <xhua89s@163.com>
2025-03-19 15:54:05 +0800

    Add files via upload

--- yuefen.py
+++ yuefen.py
@@ -8,9 +8,9 @@ by hua
 import streamlit as st
 import pandas as pd
 
-df_yf= pd.read_csv("D:\\建发\\result_yf.csv")
+df_yf= pd.read_csv("./result_yf.csv")
 st.dataframe(df_yf)
 st.divider()
-df_yf2= pd.read_csv("D:\\建发\\result_ysp.csv")
+df_yf2= pd.read_csv("./result_ysp.csv")
 st.dataframe(df_yf2)
 st.divider()
